e050337f2cb9ebe4a04f1e1d079972592464c9b3
0Xiaohei0 <15852786007ysc@gmail.com>
2020-12-09 19:27:21 -0500

    started implementing items

---
+++ Assets/Scripts/itemBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBehavior : MonoBehaviour
+{
+    [SerializeField] float flashWaitTime = 0.25f;
+    [SerializeField] float flashTime = 8f;
+    [SerializeField] float timeBeforeFlash = 8f;
+    [SerializeField] bool isFlashing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlashing)
+        {
+            StartCoroutine(Flash());
+        }
+
+    }
+    IEnumerator Flash()
+    {
+        yield return new WaitForSeconds(0.25f);
+    }
+    IEnumerator startItemCountdown()
+    {
+        yield return new WaitForSeconds(timeBeforeFlash);
+
+        yield return new WaitForSeconds(flashTime);
+    }
+}
